d0325458046d77ac651010a49820ae32a9399508
AjdinK <Ajdin.kuduzovic@gmail.com>
2023-06-03 10:22:29 +0200

    v2

--- lib/screens/products/product_list_screen.dart
+++ lib/screens/products/product_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/providers/product_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductListScreen extends StatefulWidget {
   static const String routeName = '/products';
@@ -9,11 +11,31 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  ProductProvider? _productProvider = null;
+  String data = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _productProvider = context.read<ProductProvider>();
+    loadData();
+  }
+
+  Future loadData() async {
+    var tmpData = await _productProvider?.get(null);
+    setState(
+      () {
+        data = tmpData;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("this is ProductListScreen"),
+        child: Text(data),
       ),
     );
   }
